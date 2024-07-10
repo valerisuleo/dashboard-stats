21ec5b765ce688f401c1321cad045ff374f4d572
valerisuleo <valerio.risuleo@gmail.com>
2024-07-10 16:59:14 +0100

    create hook to fetch data

---
+++ src/app/pages/dashboard/hooks/useGameLogs.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
+import { IOuts, IPlayerStats } from '../interfaces';
+import axios from 'axios';
+
+const useGameLogs = (initialValue1: number, initialValue2: number) => {
+    const [stats, setStats] = useState<IPlayerStats[]>([]);
+    const [outs, setOuts] = useState({} as IOuts);
+
+    useEffect(() => {
+        getGameLogs(initialValue1, initialValue2);
+    }, []);
+
+    async function getGameLogs(min: number, max: number) {
+        try {
+            const { data } = await axios.get('http://localhost:3000/gamelogs', {
+                params: { min, max },
+            });
+
+            console.log(data.gamelogs);
+            setStats(data.gamelogs);
+            setOuts((prev) => ({
+                ...prev,
+                progress: (data.gamelogs.length / data.total) * 100,
+            }));
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    function updateState(current: number) {
+        setOuts((prev) => ({ ...prev, average: current }));
+    }
+
+    return { stats, outs, getGameLogs, updateState };
+};
+
+export default useGameLogs;
